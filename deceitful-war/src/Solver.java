f6227925e447ebb2f218b8102c2f2a366caac86d
Shawn Clark <shawn.tuatara@gmail.com>
2014-04-12 08:13:34 -0700

    Created project for Deceitful War

---
+++ deceitful-war/src/Solver.java
@@ -0,0 +1,112 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solver {
+	private static final String PROBLEM = "d-small";
+
+	// TODO Modify for sub-class of Case
+	private List<SubCase> cases = new ArrayList<SubCase>();
+
+	private class SubCase extends Case {
+		public SubCase(int caseNumber) {
+			super(caseNumber);
+		}
+	}
+
+	// TODO Scan in a single case
+	private SubCase scanCase(BufferedReader reader, int caseNumber) throws NumberFormatException, IOException {
+		return new SubCase(caseNumber);
+	}
+
+	// TODO Solve the problem
+	public void solve() {
+		for (SubCase problemCase : cases) {
+			String solution = null;
+			problemCase.setSolution(solution);
+		}
+	}
+
+	/*
+	 * Do not need to modify anything below here
+	 */
+	private void scanInput(String inputFile) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(inputFile));
+			int numberOfCases = Integer.parseInt(reader.readLine());
+			for (int caseNumber = 1; caseNumber <= numberOfCases; caseNumber++) {
+				cases.add(scanCase(reader, caseNumber));
+			}
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void output(String outputFile) {
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+			for (Case problemCase : cases) {
+				StringBuilder stringBuilder = new StringBuilder();
+				stringBuilder.append("Case #").append(problemCase.getCaseNumber()).append(": ").append(problemCase.getSolution());
+				String caseOutput = stringBuilder.toString();
+
+				bufferedWriter.write(caseOutput);
+				bufferedWriter.newLine();
+				System.out.println(caseOutput);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bufferedWriter != null) {
+				try {
+					bufferedWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Solver solver = new Solver();
+		solver.scanInput(PROBLEM + ".in");
+		solver.solve();
+		solver.output(PROBLEM + ".out");
+	}
+
+	public class Case {
+		private final int caseNumber;
+
+		private String solution;
+
+		public Case(int caseNumber) {
+			this.caseNumber = caseNumber;
+		}
+
+		public int getCaseNumber() {
+			return caseNumber;
+		}
+
+		public void setSolution(String solution) {
+			this.solution = solution;
+		}
+
+		public String getSolution() {
+			return solution;
+		}
+	}
+}
