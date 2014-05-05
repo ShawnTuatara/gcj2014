c108d1c8a1fe3a2bb23e2f76260436bf3dc0d8cc
Shawn Clark <sclark@ea.com>
2014-05-05 11:02:46 -0700

    Move to Scanner.

--- template/src/Solver.java
+++ template/src/Solver.java
@@ -1,100 +1,93 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Solver {
-	private static final String PROBLEM = "a-small";
+    private static final String PROBLEM = "a-small";
 
-	// TODO Solve a single case
-	private ProblemCase solveCase(BufferedReader reader, int caseNumber) throws NumberFormatException, IOException {
-		ProblemCase problemCase = new ProblemCase(caseNumber);
+    // TODO Solve a single case
+    private ProblemCase solveCase(Scanner scanner, int caseNumber) {
+        ProblemCase problemCase = new ProblemCase(caseNumber);
 
-		problemCase.setSolution("");
+        problemCase.setSolution("");
 
-		return problemCase;
-	}
+        return problemCase;
+    }
 
-	/*
-	 * Do not need to modify anything below here
-	 */
-	private List<ProblemCase> cases = new ArrayList<ProblemCase>();
+    /*
+     * Do not need to modify anything below here
+     */
+    private List<ProblemCase> cases = new ArrayList<ProblemCase>();
 
-	private void solve(String inputFile) {
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(inputFile));
-			int numberOfCases = Integer.parseInt(reader.readLine());
-			for (int caseNumber = 1; caseNumber <= numberOfCases; caseNumber++) {
-				cases.add(solveCase(reader, caseNumber));
-			}
-		} catch (NumberFormatException | IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    private void solve(String inputFile) throws FileNotFoundException {
+        Scanner scanner = null;
+        scanner = new Scanner(new File(inputFile));
+        int numberOfCases = scanner.nextInt();
+        for (int caseNumber = 1; caseNumber <= numberOfCases; caseNumber++) {
+            cases.add(solveCase(scanner, caseNumber));
+        }
+    }
 
-	private void output(String outputFile) {
-		BufferedWriter bufferedWriter = null;
-		try {
-			bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
-			for (ProblemCase problemCase : cases) {
-				StringBuilder stringBuilder = new StringBuilder();
-				stringBuilder.append("Case #").append(problemCase.getCaseNumber()).append(": ").append(problemCase.getSolution());
-				String caseOutput = stringBuilder.toString();
+    private void output(String outputFile) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+            for (ProblemCase problemCase : cases) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("Case #").append(problemCase.getCaseNumber()).append(": ").append(problemCase.getSolution());
+                String caseOutput = stringBuilder.toString();
 
-				bufferedWriter.write(caseOutput);
-				bufferedWriter.newLine();
-				System.out.println(caseOutput);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (bufferedWriter != null) {
-				try {
-					bufferedWriter.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+                bufferedWriter.write(caseOutput);
+                bufferedWriter.newLine();
+                System.out.println(caseOutput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Solver solver = new Solver();
-		solver.solve(PROBLEM + ".in");
-		solver.output(PROBLEM + ".out");
-	}
+    public static void main(String[] args) {
+        Solver solver = new Solver();
+        try {
+            solver.solve(PROBLEM + ".in");
+            solver.output(PROBLEM + ".out");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public class ProblemCase {
-		private final int caseNumber;
+    public class ProblemCase {
+        private final int caseNumber;
 
-		private String solution;
+        private String solution;
 
-		public ProblemCase(int caseNumber) {
-			this.caseNumber = caseNumber;
-		}
+        public ProblemCase(int caseNumber) {
+            this.caseNumber = caseNumber;
+        }
 
-		public int getCaseNumber() {
-			return caseNumber;
-		}
+        public int getCaseNumber() {
+            return caseNumber;
+        }
 
-		public void setSolution(String solution) {
-			this.solution = solution;
-		}
+        public void setSolution(String solution) {
+            this.solution = solution;
+        }
 
-		public String getSolution() {
-			return solution;
-		}
-	}
+        public String getSolution() {
+            return solution;
+        }
+    }
 }
